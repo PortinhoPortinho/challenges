0e27ddc5c8a0b4067824f98522b566b88569906d
PortinhoPortinho <m.porto2508@gmail.com>
2026-02-06 04:36:01 -0300

    add the end of class section

---
+++ 62.js
@@ -0,0 +1,15 @@
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    value: Number(prompt("Degrees celsius:")),
+  };
+  console.table(measurement);
+  //console.log(measurement.value);
+  //console.warn(measurement.value);
+  //console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+console.log(measureKelvin());
