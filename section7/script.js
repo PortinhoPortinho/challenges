6c4a36249438ecaa09fb1fea15d90cb95104c5b5
PortinhoPortinho <m.porto2508@gmail.com>
2026-02-08 21:29:27 -0300

    add project from section 7 'guess my number'

---
+++ section7/script.js
@@ -0,0 +1,70 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+*/
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  //caso nao tenha um numero no input do botao ao clicar no check
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+
+    //caso o numero seja igual ao numero secreto ao clicar em check
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#a2c797';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > Number(document.querySelector('.highscore').textContent)) {
+      document.querySelector('.highscore').textContent = score;
+    }
+    //caso o numero seja maior que o numero secreto ao clicar em check
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('body').style.backgroundColor = '#b48585';
+      document.querySelector('.number').textContent = secretNumber;
+      document.querySelector('.score').textContent = 0;
+    }
+
+    //caso o numero seja menor que o numero secreto ao clicar em check
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('body').style.backgroundColor = '#b48585';
+      document.querySelector('.number').textContent = secretNumber;
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
