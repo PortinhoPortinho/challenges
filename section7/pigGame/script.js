70daf4995b05686e680bde186af194263bec422b
PortinhoPortinho <m.porto2508@gmail.com>
2026-02-10 22:09:18 -0300

    declare the variables before using in a function and change the blur value to 0px

--- section7/pigGame/script.js
+++ section7/pigGame/script.js
@@ -1,5 +1,21 @@
 'use strict';
 
+let playing = true;
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+let currentScore = 0;
+let activePlayer = 0;
+let scores = [0, 0];
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
 const init = function () {
   playing = true;
   scores = [0, 0];
@@ -23,25 +39,6 @@ const switchPlayer = function () {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
-let playing = true;
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
-
-let currentScore = 0;
-let activePlayer = 0;
-let scores = [0, 0];
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
-
-score0El.textContent = 0;
-score1El.textContent = 0;
-
 diceEl.classList.add('hidden');
 init();
 btnRoll.addEventListener('click', function () {
