af362c5bbd3ccc5f769a3aeff59c941bbfda8142
PortinhoPortinho <m.porto2508@gmail.com>
2026-03-22 19:56:41 -0300

    lecture 263

--- section16/script.js
+++ section16/script.js
@@ -36,7 +36,6 @@ const getCountryData = function (country) {
 };
 getCountryData('portugal');
 getCountryData('brazil');
-*/
 //////////////////////////////////////////////////
 // new lecture
 //ajax
@@ -52,15 +51,15 @@ const countriesContainer = document.querySelector('.countries');
 const renderCountry = function (data, className = '') {
   const html = `
   <article class="country ${className}">
-          <img class="country__img" src="${data.flags.png}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} million people</p>
-            <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
-            <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
-          </div>
-        </article>
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+  <h3 class="country__name">${data.name.common}</h3>
+  <h4 class="country__region">${data.region}</h4>
+  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} million people</p>
+  <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
+  <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
+  </div>
+  </article>
   `;
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
@@ -101,3 +100,15 @@ setTimeout(() => {
     }, 1000);
   }, 1000);
 }, 1000);
+*/
+
+///////////////////////////////////////
+// new lecture
+//promises and the fetch api
+
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+// request.send();
+
+const request = fetch('https://restcountries.com/v3.1/name/portugal')
+console.log(request);
