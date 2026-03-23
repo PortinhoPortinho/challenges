d53546d9d4ecf846bc701f6a23f8a2c35848ca6a
PortinhoPortinho <m.porto2508@gmail.com>
2026-03-23 08:32:02 -0300

    271 Building a Simple Promise

--- section16/script.js
+++ section16/script.js
@@ -181,11 +181,11 @@ const getCountryData = function (country) {
       renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
     });
 };
-
 btn.addEventListener('click', function () {
-  getCountryData('australia');
+  getCountryData('Brasil');
 });
 
+/*
 console.log('Test start');
 setTimeout(() => console.log('0 sec timer'), 0);
 Promise.resolve('Resolved promise 1').then(res => console.log(res));
@@ -194,3 +194,44 @@ Promise.resolve('Resolved promise 2').then(res => {
   console.log(res);
 });
 console.log('Test end');
+*/
+const loteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lotery draw is happening 🎲');
+  setTimeout(function () {
+    if (Math.random() > 0.5) {
+      resolve('🎉 You won the lotery!');
+    } else {
+      reject(new Error('😭 You lost the lotery!'));
+    }
+  }, 1000);
+});
+
+loteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(1)
+  .then(() => {
+    console.log('i waited for 1 second');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('i waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('i waited for 3 seconds');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('i waited for 4 seconds');
+    return wait(1);
+  })
+  .then(() => console.log('i waited for 5 seconds'));
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem')).catch(x => console.log(x));
