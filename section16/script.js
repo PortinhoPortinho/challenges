8c0700539096426c05abe0ef16d8b1715ba86f30
PortinhoPortinho <m.porto2508@gmail.com>
2026-03-22 21:23:50 -0300

    266 Handling rejected Promises

--- section16/script.js
+++ section16/script.js
@@ -1,8 +1,29 @@
 'use strict';
-/*
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+  <h3 class="country__name">${data.name.common}</h3>
+  <h4 class="country__region">${data.region}</h4>
+  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} million people</p>
+  <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
+  <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
+  </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+/*
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
 
@@ -41,8 +62,8 @@ getCountryData('brazil');
 */
 // new lecture
 //ajax
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+// const btn = document.querySelector('.btn-country');
+// const countriesContainer = document.querySelector('.countries');
 
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
@@ -50,23 +71,7 @@ const countriesContainer = document.querySelector('.countries');
 // NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
-const renderCountry = function (data, className = '') {
-  const html = `
-  <article class="country ${className}">
-  <img class="country__img" src="${data.flags.png}" />
-  <div class="country__data">
-  <h3 class="country__name">${data.name.common}</h3>
-  <h4 class="country__region">${data.region}</h4>
-  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} million people</p>
-  <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
-  <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
-  </div>
-  </article>
-  `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
-
+/*
 ///////////////////////////////////////
 const getCountryAndNeighbor = function (country) {
   const request = new XMLHttpRequest();
@@ -88,7 +93,6 @@ const getCountryAndNeighbor = function (country) {
     });
   });
 };
-/*
 // getCountryAndNeighbour('portugal');
 getCountryAndNeighbor('brazil');
 setTimeout(() => {
@@ -113,21 +117,27 @@ setTimeout(() => {
 // request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
 // request.send();
 
-const request = fetch('https://restcountries.com/v3.1/name/portugal')
+const request = fetch('https://restcountries.com/v3.1/name/portugal');
 console.log(request);
 // country 1
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
     .then(data => {
-      renderCountry(data[0]); 
-    const neighbor = data[0].borders?.[0];
-    if (!neighbor) return;
-    //country 2 - neighbor
-    return fetch(`https://restcountries.com/v3.1/alpha/${neighbor}`);
-  })
-      .then(response => response.json())
-      .then(data => renderCountry(data[0], 'neighbor'));
-  };
-  getCountryData('portugal');
+      renderCountry(data[0]);
+      const neighbor = data[0].borders[0];
+      if (!neighbor) return;
+      //country 2 - neighbor
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbor}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbor'))
+    .catch(err => {
+      console.error(`${err} 💥💥💥`);
+      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+    });
+};
+
+btn.addEventListener('click', function () {
   getCountryData('brazil');
+});
