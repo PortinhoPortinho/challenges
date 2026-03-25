c09a5c9dff8c3da5534b0bb85049c9e04e32f580
PortinhoPortinho <m.porto2508@gmail.com>
2026-03-25 08:27:31 -0300

    285 top level await

--- section17/script.js
+++ section17/script.js
@@ -16,3 +16,23 @@ console.log('importing module');
 add('apple', 2);
 add('banana', 3);
 console.log(cart);
+//////////////////////////////////////////////////////////
+//new lecture
+// console.log('start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+const lastPost = getLastPost();
+console.log(lastPost);
+// not very clean
+lastPost.then(last => console.log(last));
+
+const getLastPost2 = await getLastPost();
+console.log(getLastPost2);
--- section17/shoppingcart.js
+++ section17/shoppingcart.js
@@ -1,5 +1,8 @@
 //exporting module
 console.log('exporting module');
+console.log('start fetching users');
+const res = await fetch('https://jsonplaceholder.typicode.com/users');
+console.log('fetched users');
 const shippingCost = 10;
 export const cart = [];
 export const addToCart = function (product, quantity) {
